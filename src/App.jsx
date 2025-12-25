f1a00fae6adf2f371f0e7c203babfbc12d20808d
mumtahina324-alt <mumtahina324@gmail.com>
2025-12-25 19:53:15 +0600

    amra onek kaj korechi

--- src/App.jsx
+++ src/App.jsx
@@ -12,3 +12,5 @@ export default function App() {
 // aj k amra muloto janbo kokhn amra state ar value string nebo, kokhn array nebo, kokhn object nebo ei bisooi gulo
 // eitar jonno 5 ta rules ache, cholun amrA eigulo explore kori
 // accha prothomei ache group related state , eita amra ekta example ar maddhome bujhbo
+// akhn dekhun jokhn e ami ei file a likhtechi and save kortechi eita git track korbe
+// akhn dhorun ekhane ami onk kaj korlam, korar por seita k git hub a push korbo
